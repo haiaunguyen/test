4c840f0a584400523d4bc8fa4668f311a7cd7d57
haiaunguyen <haiau8520@gmail.com>
2017-08-21 08:48:21 +0700

    Add header

--- header.php
+++ header.php
@@ -26,7 +26,7 @@
                 <div class="col-xs-12">
 
                     <nav class="navbar navbar-default navbar-fixed-top">
-                        <div class="container" style="height: 100px, background-color: ">
+                        <div class="container" style="height: 100px ">
                             <!-- Brand and toggle get grouped for better mobile display -->
                             <div class="navbar-header">
                             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">                          
